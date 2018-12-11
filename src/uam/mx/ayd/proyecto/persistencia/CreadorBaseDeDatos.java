1891329da14a76b9ade597525f54ff356867fff8
Christian Ceciliano <cristiancehi@gmail.com>
2018-12-10 22:11:52 -0600

    Base de Datos Arreglada

--- src/uam/mx/ayd/proyecto/persistencia/CreadorBaseDeDatos.java
+++ src/uam/mx/ayd/proyecto/persistencia/CreadorBaseDeDatos.java
@@ -12,9 +12,9 @@ public class CreadorBaseDeDatos {
         try{
             Connection conexion = ManejadorBD.dameConexion();
             Statement statement = conexion.createStatement();
-            statement.execute("create table Medelos(IDmodelo INTEGER PRYMARY KEY not null, "
-                    + "descripcion varchar (40), precio double, color varchar(20), "
-                    + "talla varchar(10), piezas int, reglaventa varchar(30)");
+            statement.execute("create table Modelos(IDmodelo INTEGER PRIMARY KEY not null, "
+                    + "descripcion varchar(40), precio double, color varchar(20), "
+                    + "talla varchar(10), piezas int, reglaventa varchar(30))");
             System.out.println("Taba Modelos creada");
             //statement.execute("create table Ventas(ID venta INTEGER PRYMARY KEY not null,"        + "")
             ManejadorBD.termina();
